2982a436809be448b67662bb8c528b0731703e7d
lucasm73 <56769716+lucasm73@users.noreply.github.com>
2019-10-19 14:41:26 -0300

    Add files via upload

---
+++ materializecss/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+  <html lang="pt-br">
+
+    <head>
+
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="estilo.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta charset="utf-8">
+
+    </head>
+
+    <body>
+          <nav>
+    <div class="nav-wrapper blue darken-4">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>      
+
+
+         
+
+
+
+      <script type="text/javascript" src="js/jquery.js"></script><!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="app.js"></script>
+
+    </body>
+    
+  </html>
